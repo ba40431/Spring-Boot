180886c05f267e6ab5ab96b10fda30710e2301cf
abby <ba40431@gmail.com>
2024-05-06 19:35:16 +0800

    use Value 2024-05-06

--- demo/src/main/java/com/example/demo/HpPrinter.java
+++ demo/src/main/java/com/example/demo/HpPrinter.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import jakarta.annotation.PostConstruct;
@@ -7,12 +8,8 @@ import jakarta.annotation.PostConstruct;
 @Component
 public class HpPrinter implements Printer {
 
+    @Value("${count:10}") //如果想要「避免 application.properties 中找不到該 key，導致 Spring Boot 程式運行失敗」的狀況的話，那麼就可以在 @Value 中使用 :，在後面加上預設值，當找不到 key 時，就可以直接無縫接軌改成使用預設值來運行
     private int count;
-
-    @PostConstruct
-    public void initialize(){
-        count = 5;
-    }
 
     @Override
     public void print(String message){

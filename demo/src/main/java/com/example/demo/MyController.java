fa6aa35c37ff6dec1407decd0dca3525454b2804
abby <ba40431@gmail.com>
2024-05-08 17:12:21 +0800

    add product & user url 2024-05-08

--- demo/src/main/java/com/example/demo/MyController.java
+++ demo/src/main/java/com/example/demo/MyController.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +20,24 @@ public class MyController {
         printer.print("Hello World !");
         // System.out.println("Hi!");
         return "Hello World!";
+    }
+
+    @RequestMapping("/product")
+    public Store product() {
+        Store store = new Store();
+        List<String> list = new ArrayList<>();
+        list.add("蘋果");
+        list.add("橘子");
+        store.setProductList(list);
+
+        return store;
+    }
+
+    @RequestMapping("/user")
+    public Student user() {
+        Student student = new Student();
+        student.setName("Judy");
+        return student;
     }
 
 }
